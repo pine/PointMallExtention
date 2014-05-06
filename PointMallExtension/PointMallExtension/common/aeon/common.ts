d457ab5ac58008cf576b948154b6ee308095407e
MIZUNE Pine <pinemz@gmail.com>
2014-05-06 18:05:47 +0900

    Supported aeon card

---
+++ PointMallExtension/PointMallExtension/common/aeon/common.ts
@@ -0,0 +1,4 @@
+﻿var POINT_MALL_NAME = "ときめきポイントTOWN";
+var POINT_MALL_URL = "http://www.aeon.co.jp/tpt/";
+var POINT_MALL_SHOPS_JSON_URL =
+    "https://raw.githubusercontent.com/pine613/PointMallShopLink/master/list/aeon/v1/list.json";
