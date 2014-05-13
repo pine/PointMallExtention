db442e7dc3b031e2c450ad77018f42746ae42e61
MIZUNE Pine <pinemz@gmail.com>
2014-05-14 04:58:32 +0900

    Add smbc define files

---
+++ PointMallExtension/PointMallExtension/common/smbc/common.ts
@@ -0,0 +1,6 @@
+﻿var CREDIT_CARD_ISSUER = "Smbc";
+
+var POINT_MALL_NAME = "ポイントUPモール";
+var POINT_MALL_URL = "https://mall.smbc-card.com";
+var POINT_MALL_SHOPS_JSON_URL =
+    "https://raw.githubusercontent.com/pine613/PointMallShopLink/master/list/smbc/v1/list.json";
